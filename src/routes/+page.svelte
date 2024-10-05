f9ae065e07e75748bd978847944f4e50eda12c78
William Hughes <william@williamjhughes.com>
2024-10-05 23:08:09 +0100

    add about.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
+	import About from '$lib/components/About.svelte';
 	import Header from '$lib/components/Header.svelte';
 </script>
 
-<main class="container p-6 md:p-16">
+<main class="container space-y-8 p-6 md:p-16">
 	<Header />
+	<About />
 </main>
